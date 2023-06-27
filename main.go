4777feab355c5597de40957b1d3455c4a95767d9
Hassan El Abdallah <hassankh613@gmail.com>
2023-06-27 17:31:10 +0300

    Test sort and search algorithms

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"ds/algorithms"
-	"ds/non_linear_ds"
 	"fmt"
 )
 
@@ -12,20 +11,25 @@ func main() {
 	// var ls linear_ds.SLinkedList = linear_ds.SLinkedList{}
 	// var dls linear_ds.DLinkedList = linear_ds.DLinkedList{}
 
-	var g non_linear_ds.Graph = *non_linear_ds.CreateGraph(6)
+	// var g non_linear_ds.Graph = *non_linear_ds.CreateGraph(6)
+	var arr = []int{3, 4, 2, 1, 10, 6}
 
-	g.AddEdge(0, 1)
-	g.AddEdge(0, 2)
-	g.AddEdge(1, 3)
-	g.AddEdge(1, 4)
-	g.AddEdge(2, 4)
-	g.AddEdge(3, 4)
-	g.AddEdge(3, 5)
-	g.AddEdge(4, 5)
+	algorithms.BubbleSort(arr)
 
-	g.PrintGraph()
+	fmt.Println(arr)
+
+	// g.AddEdge(0, 1)
+	// g.AddEdge(0, 2)
+	// g.AddEdge(1, 3)
+	// g.AddEdge(1, 4)
+	// g.AddEdge(2, 4)
+	// g.AddEdge(3, 4)
+	// g.AddEdge(3, 5)
+	// g.AddEdge(4, 5)
+
+	// g.PrintGraph()
 	//algorithms.BFS(g, 0)
-	algorithms.DFS(g, 0)
+	// algorithms.DFS(g, 0)
 	// q.Enqueue(1)
 	// ls.Push(1)
 	// dls.InsertFirst(1)
